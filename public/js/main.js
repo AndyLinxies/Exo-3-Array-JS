0934eba87ba2e10e1224c8bb35c6ad66584cee09
AndyLinxies <linxies@gmail.com>
2021-04-16 16:38:05 +0200

    Exo 4 Array

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let maVar = ['Coding 11','Coding 12','Coding 13'];
+console.log(maVar);
+maVar.push('Andy');
+console.log(maVar);
+maVar.splice(1,1,'Coding18') //Cod12 a l'index 1 est supprimé et on met Cod18 a la place
+console.log(maVar);
+maVar.splice(1,1);
+console.log(maVar);
